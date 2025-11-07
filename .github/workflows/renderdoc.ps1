598208203ca38bfd7d7438253f108170cc5f46fa
Mark Raynsford <code@io7m.com>
2025-11-07 20:55:21 +0000

    Replace with hardcoded 1.41.

--- .github/workflows/renderdoc.ps1
+++ .github/workflows/renderdoc.ps1
@@ -1,16 +1,4 @@
-$release = Invoke-RestMethod -Uri 'https://api.github.com/repos/baldurk/renderdoc/releases/latest' -UseBasicParsing
-$asset = $release.assets | Where-Object { $_.name -match 'RenderDoc_.*_64\.msi$' } | Select-Object -First 1
-if (-not $asset) { Write-Error 'RenderDoc MSI asset not found in latest release'; exit 1 }
-
-Write-Host "Downloading $($asset.name)..."
-Invoke-WebRequest -Uri $asset.browser_download_url -OutFile renderdoc.msi -UseBasicParsing
-
-Start-Process -FilePath msiexec.exe -Wait -ArgumentList '/i', (Resolve-Path renderdoc.msi), '/qn', '/norestart'
-
-$exe = 'C:\Program Files\RenderDoc\qrenderdoc.exe'
-if (-Not (Test-Path $exe)) {
-  Write-Error "qrenderdoc.exe not found at expected path ($exe)"
-  exit 1
-}
-
-& $exe --version
+$uri = 'https://renderdoc.org/stable/1.41/RenderDoc_1.41_64.msi'
+Invoke-WebRequest -Uri $uri -OutFile renderdoc.msi -UseBasicParsing
+Start-Process msiexec.exe -Wait -ArgumentList '/i', (Resolve-Path renderdoc.msi), '/qn', '/norestart'
+& 'C:\Program Files\RenderDoc\qrenderdoc.exe' --version
