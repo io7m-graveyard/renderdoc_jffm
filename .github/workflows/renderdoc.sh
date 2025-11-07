c4f5201ccb3e95038a80183d354d75fcc5f83c7e
Mark Raynsford <code@io7m.com>
2025-11-07 20:38:34 +0000

    Try new workflows.

--- .github/workflows/renderdoc.sh
+++ .github/workflows/renderdoc.sh
@@ -1,30 +1,14 @@
 #!/bin/sh -ex
 
+RENDERDOC_VERSION="1.41"
+
 (cat <<EOF
------BEGIN PGP SIGNATURE-----
-
-iQIzBAABCgAdFiEEGwOduaRxii1pneAxrGEsMSDDRpUFAmkLP7oACgkQrGEsMSDD
-RpV6kBAAhSnOm/BH956S53ZeQ8AU/pQ/gm+ONtMItkle4F1geO2rv0tOttHudq4M
-ZvAlkL58k3IfIKMWOh0eLKCVR5Ljz+nvWHYtemNNFe1vI+wHE4wHuac/hB1DoQaP
-8oXwZPYDa6jd93LPCS6b/oTeewSdQu2ZjaPz5XTbG/K9NOghGcidqayXm8X6A4ZI
-eOIQS4jvlTwYI9Ef0N/U6Adl2gd/6xrXeDssW887SPnSOKP7fCdO2M8gwO7S4Rrw
-UgWHyV3M+WnpLrCLDhrsc0q/FqE5P05/zJy030lJCeL9QzqRsPBsqxpNTitXjkxA
-4nvhQkglBCmUqlJH76nBZhH/9TLpyN9mgDgJoPVGB/mSJhVEAJCqAYxr7U1BDIYq
-9n+JiCoqqIZDwValIfVsxHfMHIRg99XjTLMs3elIymhGsBaNHk9va40XorY5L9ie
-AYqJ7nID4BV2IkeJ9VfZGXfW0C6+XaLlrRN8avc+1pKrsJEhSHFfmlaHYbY09Vx7
-Czb4mBpRV7L9OJ/HPpwuYbzFlWNJPykcMclAh2PEP07gT0HGqP9Nj8JYqOLuPNfS
-ssZtQ3LpmWWooWkHQHgg6HHIMee7tWhRWWFyfia3KY6Z9FOXORuVjpmRIbh8aJte
-+QL2K9QFftOLeut1oePlkEVrDJPCPTVGT+eeJfqQ6c31ZHDra7g=
-=eeDb
------END PGP SIGNATURE-----
+5b64338eeafc755737d89136118dc3820b5cbd8a87b958547596cb911f54a481  renderdoc.tar.gz
 EOF
-) > renderdoc.tar.gz.sig
+) > renderdoc.tar.gz.sha256
 
-wget -O renderdoc.tar.gz https://renderdoc.org/stable/1.41/renderdoc_1.41.tar.gz
-
-gpg --verify renderdoc.tar.gz.sig
-
-tar -x -z -f renderdoc.tar.gz --strip-components=2 renderdoc_1.41/lib/librenderdoc.so
-
+wget -O renderdoc.tar.gz "https://renderdoc.org/stable/${RENDERDOC_VERSION}/renderdoc_${RENDERDOC_VERSION}.tar.gz"
+sha256 -c renderdoc.tar.gz.sha256
+tar -x -z -f renderdoc.tar.gz --strip-components=2 "renderdoc_${RENDERDOC_VERSION}/lib/librenderdoc.so"
 rm -f renderdoc.tar.gz
 rm -f renderdoc.tar.gz.sig
